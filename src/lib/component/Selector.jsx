5e3d7a3221c99b870537ce558d5f33ea5fa7c107
m-delacre <mdelacre@protonmail.com>
2023-01-27 23:38:42 +0100

    ajout commentaires

--- src/lib/component/Selector.jsx
+++ src/lib/component/Selector.jsx
@@ -1,10 +1,23 @@
 import "./Selector.css";
 import React from "react";
-
+/**
+ * 
+ * @param {string} props.value 
+ * 
+ * @returns will create an option for the select
+ */
 function ListItem(props){
     return <option value={props.value}>{props.value}</option>
 }
 
+/**
+ * 
+ * @param {string} title 
+ * @param {Array} valuesList
+ * @param {function} onChange(event)
+ * 
+ * @returns our Selector component with the label and the select
+ */
 function Selector({title,valuesList, onChange}){
     const listOption = valuesList.map((option, index)=>
         <ListItem key={index} value={option.name}/>
